a30a3e82d97d012e515623a3f9055e2edcb70bb3
Suresh <98641509+Suresh-Ben@users.noreply.github.com>
2023-06-09 00:51:22 +0530

    Test Completed

---
+++ PrismForce-test.js
@@ -0,0 +1,69 @@
+//Imports
+const readline = require("readline");
+
+//Init
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+//Variables
+var input = {};
+var output = {};
+let maxMonth = 0;
+let year = "";
+
+var processInput = () => {
+    return new Promise((res, rej) => {
+        rl.question("Please enter the input Object: ", (inputObj) => {
+            res(JSON.parse(inputObj));
+        })
+    })
+};
+
+var processData = () => {
+    let expenses = input["expenseData"];
+    let revenue = input["revenueData"];
+
+    expenses.map(data => {
+        const date = new Date(data["startDate"]);
+        let month = date.getMonth();
+        if (month > maxMonth) maxMonth = month;
+        year = date.getFullYear();
+        let amount = data["amount"];
+
+        if (output[month] == undefined) output[month] = 0;
+        output[month] -= amount;
+    });
+
+    revenue.map(data => {
+        const date = new Date(data["startDate"]);
+        let month = date.getMonth();
+        if (month > maxMonth) maxMonth = month;
+        year = date.getFullYear();
+        let amount = data["amount"];
+
+        if (output[month] == undefined) output[month] = 0;
+        output[month] += amount;
+    });
+};
+
+var processOutput = () => {
+    let result = { balance: [] };
+    for (let i = 0; i <= maxMonth; i++) {
+        result["balance"].push({
+            amount: output[i] != undefined ? output[i] : 0,
+            startDate: year + "-" + (i + 1) + "-01T00:00:00.000Z"
+        });
+    }
+
+    console.log("\n\n\nOutput of the data: \n", result);
+};
+
+//process
+async function Process() {
+    input = await processInput();
+    processData();
+    processOutput();
+};
+Process();
